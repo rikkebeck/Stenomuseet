eb814fc430fe172b3a6a64f85819eb404e039d80
rikkebeck <eaa24ribk@students.eaaa.dk>
2025-05-21 11:06:28 +0200

    Add files via upload

--- anatomi.js
+++ anatomi.js
@@ -8,37 +8,37 @@ const maelkegange = document.getElementById('maelkegange');
 const lymfesystem = document.getElementById('lymfesystem');
 
 // Henter fade-elementet som skal få fade-out klassen ved klik:
-const fadeElement = document.getElementById('fadeEffekt'); 
+const fadeElement = document.getElementById('fadeEffekt');
 
 function fadeOutAndNavigate(event) {
-    event.preventDefault(); // Forhindrer standard navigasjon
-    fadeElement.classList.add('fade-out');
-    const targetHref = this.getAttribute('href'); // 'this' refererer til det klikkede elementet
-    setTimeout(() => {
-      window.location.href = targetHref; // Naviger etter fade-out
-    }, 500); // Samme varighet som CSS-overgangen
-  }
-  
-  if (fedtvaev) {
-    fedtvaev.onclick = fadeOutAndNavigate;
-  }
-  
-  if (brystvorte) {
-    brystvorte.onclick = fadeOutAndNavigate;
-  }
+  event.preventDefault(); // Forhindrer standard navigasjon
+  fadeElement.classList.add('fade-out');
+  const targetHref = this.getAttribute('href'); // 'this' refererer til det klikkede elementet
+  setTimeout(() => {
+    window.location.href = targetHref; // Naviger etter fade-out
+  }, 500); // Samme varighet som CSS-overgangen
+}
 
-  if (maelkekirtler) {
-    maelkekirtler.onclick = fadeOutAndNavigate;
-  }
+if (fedtvaev) {
+  fedtvaev.onclick = fadeOutAndNavigate;
+}
 
-  if (maelkegange) {
-    maelkegange.onclick = fadeOutAndNavigate;
-  }
+if (brystvorte) {
+  brystvorte.onclick = fadeOutAndNavigate;
+}
 
-  if (lymfesystem) {
-    lymfesystem.onclick = fadeOutAndNavigate;
-  }
+if (maelkekirtler) {
+  maelkekirtler.onclick = fadeOutAndNavigate;
+}
 
-  if (areola) {
-    areola.onclick = fadeOutAndNavigate;
-  }
+if (maelkegange) {
+  maelkegange.onclick = fadeOutAndNavigate;
+}
+
+if (lymfesystem) {
+  lymfesystem.onclick = fadeOutAndNavigate;
+}
+
+if (areola) {
+  areola.onclick = fadeOutAndNavigate;
+}
