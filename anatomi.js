f3522fc522bbe9e04ecbea3fcb5496849062e04a
rikkebeck <eaa24ribk@students.eaaa.dk>
2025-05-22 15:37:06 +0200

    Add files via upload

--- anatomi.js
+++ anatomi.js
@@ -1,5 +1,4 @@
 // FADE EFFEKT
-
 // Henter de forskellige knapper til undersiderne
 const fedtvaev = document.getElementById('fedtvaev');
 const brystvorte = document.getElementById('brystvorte');
@@ -8,37 +7,37 @@ const maelkegange = document.getElementById('maelkegange');
 const lymfesystem = document.getElementById('lymfesystem');
 
 // Henter fade-elementet som skal få fade-out klassen ved klik:
-const fadeElement = document.getElementById('fadeEffekt');
+const fadeElement = document.getElementById('fadeEffekt'); 
 
 function fadeOutAndNavigate(event) {
-  event.preventDefault(); // Forhindrer standard navigasjon
-  fadeElement.classList.add('fade-out');
-  const targetHref = this.getAttribute('href'); // 'this' refererer til det klikkede elementet
-  setTimeout(() => {
-    window.location.href = targetHref; // Naviger etter fade-out
-  }, 500); // Samme varighet som CSS-overgangen
-}
+    event.preventDefault(); // Forhindrer standard navigasion
+    fadeElement.classList.add('fade-out'); // Tilføjer klassen til elementet
+    const targetHref = this.getAttribute('href'); // 'this' refererer til det klikkede elementet
+    setTimeout(() => {
+      window.location.href = targetHref; // Navigerer efter klik
+    }, 500); // Samme varighed som CSS overgangen
+  }
+  
+  if (fedtvaev) {
+    fedtvaev.onclick = fadeOutAndNavigate; //klikker på dette element, vil funktionen fadeOutAndNavigate blive kørt.
+  }
+  
+  if (brystvorte) {
+    brystvorte.onclick = fadeOutAndNavigate;
+  }
 
-if (fedtvaev) {
-  fedtvaev.onclick = fadeOutAndNavigate;
-}
+  if (maelkekirtler) {
+    maelkekirtler.onclick = fadeOutAndNavigate;
+  }
 
-if (brystvorte) {
-  brystvorte.onclick = fadeOutAndNavigate;
-}
+  if (maelkegange) {
+    maelkegange.onclick = fadeOutAndNavigate;
+  }
 
-if (maelkekirtler) {
-  maelkekirtler.onclick = fadeOutAndNavigate;
-}
+  if (lymfesystem) {
+    lymfesystem.onclick = fadeOutAndNavigate;
+  }
 
-if (maelkegange) {
-  maelkegange.onclick = fadeOutAndNavigate;
-}
-
-if (lymfesystem) {
-  lymfesystem.onclick = fadeOutAndNavigate;
-}
-
-if (areola) {
-  areola.onclick = fadeOutAndNavigate;
-}
+  if (areola) {
+    areola.onclick = fadeOutAndNavigate;
+  }
